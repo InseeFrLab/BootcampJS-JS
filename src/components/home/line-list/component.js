3486d2e137490f1cd373be9ed73425234d0a40f8
Nicolas Laval <nicolas.laval@insee.fr>
2019-12-02 13:52:36 +0100

    useEffect

--- src/components/home/line-list/component.js
+++ src/components/home/line-list/component.js
@@ -1,63 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { PropTypes } from 'prop-types';
 import Table from 'components/shared/table';
 import { filterArray, sortArrayByKey } from 'utils/array-utils';
 import { getDesignations } from 'utils/call-api';
 import { Loader, Message } from 'semantic-ui-react';
 
-class List extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = { list: [], loading: true, error: undefined };
-	}
+const List = ({ search }) => {
+	const [list, setList] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(undefined);
 
-	componentDidMount() {
+	useEffect(() => {
 		getDesignations()
-			.then(res => this.setState({ list: res, loading: false }))
-			.catch(error => this.setState({ error, loading: false }));
-	}
+			.then(listAPI => {
+				setList(listAPI);
+				setLoading(false);
+			})
+			.catch(errorAPI => {
+				setError(errorAPI);
+				setLoading(false);
+			});
+	}, []);
 
-	render() {
-		const { search } = this.props;
-		const { list, error, loading } = this.state;
-		if (loading)
-			return (
-				<Loader active inline="centered" size="massive">
-					{`Chargement en cours`}
-				</Loader>
-			);
-		if (error !== undefined) {
-			return (
-				<Message
-					icon="world"
-					header="Problème avec l'API"
-					negative
-					content={error.message}
-				/>
-			);
-		}
-		const sortedList = sortArrayByKey('designation')(list);
-		const filteredArray = filterArray(sortedList)(search).map(a => ({
-			...a,
-			redList: a.redList ? 'Oui' : 'Non',
-		}));
-		const columns = [
-			{ accessor: 'designation', label: 'Désignation' },
-			{ accessor: 'phoneNumber', label: 'N° de téléphone', center: true },
-			{ accessor: 'redList', label: 'Liste rouge', center: true },
-			{ accessor: 'office', label: 'N° de bureau', center: true },
-			{ accessor: 'observation', label: 'Commentaire' },
-		];
+	if (loading)
 		return (
-			<Table
-				columns={columns}
-				data={filteredArray}
-				options={['edit']}
-				itemsPerPage={5}
+			<Loader active inline="centered" size="massive">
+				{`Chargement en cours`}
+			</Loader>
+		);
+	if (error !== undefined) {
+		return (
+			<Message
+				icon="world"
+				header="Problème avec l'API"
+				negative
+				content={error.message}
 			/>
 		);
 	}
-}
+	const sortedList = sortArrayByKey('designation')(list);
+	const filteredArray = filterArray(sortedList)(search).map(a => ({
+		...a,
+		redList: a.redList ? 'Oui' : 'Non',
+	}));
+	const columns = [
+		{ accessor: 'designation', label: 'Désignation' },
+		{ accessor: 'phoneNumber', label: 'N° de téléphone', center: true },
+		{ accessor: 'redList', label: 'Liste rouge', center: true },
+		{ accessor: 'office', label: 'N° de bureau', center: true },
+		{ accessor: 'observation', label: 'Commentaire' },
+	];
+	return (
+		<Table
+			columns={columns}
+			data={filteredArray}
+			options={['edit']}
+			itemsPerPage={5}
+		/>
+	);
+};
+
 export default List;
 
 List.propTypes = {
